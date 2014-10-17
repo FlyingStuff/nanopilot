4f0463c2c2c7dc81fd46c343a302d71bcba1fab7
Patrick Spieler <stapelzeiger@gmail.com>
2014-10-17 22:49:02 +0200

    sd detect test

--- src/main.c
+++ src/main.c
@@ -33,37 +33,37 @@ static THD_FUNCTION(heartbeat, arg)
 
 #define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
 static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]) {
-  size_t n, size;
+    size_t n, size;
 
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: mem\r\n");
-    return;
-  }
-  n = chHeapStatus(NULL, &size);
-  chprintf(chp, "core free memory : %u bytes\r\n", chCoreStatus());
-  chprintf(chp, "heap fragments   : %u\r\n", n);
-  chprintf(chp, "heap free total  : %u bytes\r\n", size);
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: mem\r\n");
+        return;
+    }
+    n = chHeapStatus(NULL, &size);
+    chprintf(chp, "core free memory : %u bytes\r\n", chCoreStatus());
+    chprintf(chp, "heap fragments   : %u\r\n", n);
+    chprintf(chp, "heap free total  : %u bytes\r\n", size);
 }
 
 static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
-  static const char *states[] = {CH_STATE_NAMES};
-  thread_t *tp;
+    static const char *states[] = {CH_STATE_NAMES};
+    thread_t *tp;
 
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: threads\r\n");
-    return;
-  }
-  chprintf(chp, "    addr    stack prio refs     state\r\n");
-  tp = chRegFirstThread();
-  do {
-    chprintf(chp, "%08lx %08lx %4lu %4lu %9s\r\n",
-             (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
-             (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-             states[tp->p_state]);
-    tp = chRegNextThread(tp);
-  } while (tp != NULL);
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: threads\r\n");
+        return;
+    }
+    chprintf(chp, "    addr    stack prio refs     state\r\n");
+    tp = chRegFirstThread();
+    do {
+        chprintf(chp, "%08lx %08lx %4lu %4lu %9s\r\n",
+                 (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
+                 (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
+                 states[tp->p_state]);
+        tp = chRegNextThread(tp);
+    } while (tp != NULL);
 }
 
 static const ShellCommand commands[] = {
@@ -108,5 +108,11 @@ int main(void)
             }
         }
         chThdSleepMilliseconds(500);
+
+        if (palReadPad(GPIOC, GPIOC_SDCARD_DETECT)) {
+            palClearPad(GPIOB, GPIOB_LED_SDCARD);
+        } else {
+            palSetPad(GPIOB, GPIOB_LED_SDCARD);
+        }
     }
 }
