a21ccbd3de807d43a8dee3acea428171eea7430f
Patrick Spieler <stapelzeiger@gmail.com>
2014-10-24 15:34:56 +0200

    tools.mk add reset command

--- tools.mk
+++ tools.mk
@@ -1,3 +1,9 @@
-
+.PHONY: flash
 flash: all
 	openocd -f oocd.cfg -c "program build/ins-board.elf verify reset"
+
+.PHONY: r
+r: reset
+.PHONY: reset
+reset:
+	openocd -f oocd.cfg -c "init" -c "reset" -c "shutdown"
