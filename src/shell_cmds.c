afdbfba084018ee2553e4c0e2924e88dce7b61cd
Patrick Spieler <stapelzeiger@gmail.com>
2015-02-26 16:12:15 +0100

    shell command threads prints name

--- src/shell_cmds.c
+++ src/shell_cmds.c
@@ -34,13 +34,13 @@ static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
         chprintf(chp, "Usage: threads\r\n");
         return;
     }
-    chprintf(chp, "    addr    stack prio refs     state\r\n");
+    chprintf(chp, "    addr    stack prio refs     state name\r\n");
     tp = chRegFirstThread();
     do {
-        chprintf(chp, "%08lx %08lx %4lu %4lu %9s\r\n",
+        chprintf(chp, "%08lx %08lx %4lu %4lu %9s %s\r\n",
                  (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
                  (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-                 states[tp->p_state]);
+                 states[tp->p_state], tp->p_name);
         tp = chRegNextThread(tp);
     } while (tp != NULL);
 }
