1c718ec44a4bcdf5f5ead382b2d9274fb38b97d6
Patrick Spieler <stapelzeiger@gmail.com>
2015-03-20 14:43:36 +0100

    git version include branch name

--- git_revision.sh
+++ git_revision.sh
@@ -3,4 +3,5 @@
 echo "// AUTOMATICALLY GENERATED" > src/git_revision.c
 echo "const char * build_git_version = \"$(git describe --dirty --always --tags)\";" >> src/git_revision.c
 echo "const char * build_git_sha = \"$(git rev-parse HEAD)\";" >> src/git_revision.c
+echo "const char * build_git_branch = \"$(git rev-parse --abbrev-ref HEAD)\";" >> src/git_revision.c
 echo "const char * build_date = \"$(date -u +"%Y-%m-%dT%H:%M:%SZ")\";" >> src/git_revision.c
