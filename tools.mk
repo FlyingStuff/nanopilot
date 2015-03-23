dc3eb076337d4a3e26740084db84535946fb06fb
Patrick Spieler <stapelzeiger@gmail.com>
2015-03-23 23:07:20 +0100

    tools.mk add dfu bootloader

--- tools.mk
+++ tools.mk
@@ -7,3 +7,7 @@ r: reset
 .PHONY: reset
 reset:
 	openocd -f oocd.cfg -c "init" -c "reset" -c "shutdown"
+
+.PHONY: dfu
+dfu: all
+	dfu-util -d 0483:df11 -c 1 -i 0 -a 0 --dfuse-address 0x08000000 -R -D build/ins-board.bin
