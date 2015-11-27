d8fb768db1b079a7a23c6c2822f0c9637a1d27e5
Patrick Spieler <stapelzeiger@gmail.com>
2015-11-27 19:43:37 +0100

    increase shell prio

--- src/thread_prio.h
+++ src/thread_prio.h
@@ -6,7 +6,7 @@
 // use priorities in the range of 2 to 127, higher number means higher priority
 
 #define THD_PRIO_LED                                    2
-#define THD_PRIO_SHELL                                  3
+#define THD_PRIO_SHELL                                  100
 #define THD_PRIO_STREAM                                 4
 #define THD_PRIO_RC_SUMD_IN                             9
 #define THD_PRIO_SENSOR_DRV_I2C                         10
