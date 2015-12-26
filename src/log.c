88a0db3a11a42434e65ecd10e3cc27c36241a894
Patrick Spieler <stapelzeiger@gmail.com>
2015-12-26 07:12:15 +0100

    write log to buffer before writing to stdout

--- src/log.c
+++ src/log.c
@@ -1,14 +1,14 @@
 #include <hal.h>
 #include <chprintf.h>
+#include <memstreams.h>
 #include "timestamp/timestamp.h"
 #include "main.h"
 
 #include "log.h"
 
-
 MUTEX_DECL(log_lock);
 
-static void write_entry_format(const char *loglevel)
+static void write_entry_format(BaseSequentialStream *stream, const char *loglevel)
 {
     const char *thread_name = "";
     if (ch.rlist.r_current != NULL && ch.rlist.r_current->p_name != NULL) {
@@ -18,7 +18,7 @@ static void write_entry_format(const char *loglevel)
     uint32_t ts = timestamp_get();
     uint32_t s = ts / 1000000;
     uint32_t us = ts - s * 1000000;
-    chprintf(stdout, LOG_COLOR_BLUE "[%4d.%06d] %s: %s" LOG_COLOR_CLEAR,
+    chprintf(stream, LOG_COLOR_BLUE "[%4d.%06d] %s: %s" LOG_COLOR_CLEAR,
              s, us, thread_name, loglevel);
 }
 
@@ -26,15 +26,20 @@ static void write_entry_format(const char *loglevel)
 void log_message(const char *lvl, const char *fmt, ...)
 {
     chMtxLock(&log_lock);
+    static uint8_t writebuf[200];
+    static MemoryStream writebuf_stream;
+    msObjectInit(&writebuf_stream, writebuf, sizeof(writebuf), 0);
 
     va_list args;
-    write_entry_format(lvl);
+    write_entry_format((BaseSequentialStream *)&writebuf_stream, lvl);
 
     va_start(args, fmt);
-    chvprintf(stdout, fmt, args);
+    chvprintf((BaseSequentialStream *)&writebuf_stream, fmt, args);
     va_end(args);
 
-    chprintf(stdout, "\n");
+    chprintf((BaseSequentialStream *)&writebuf_stream, "\n");
+
+    chSequentialStreamWrite(stdout, writebuf, writebuf_stream.eos);
 
     chMtxUnlock(&log_lock);
 }
