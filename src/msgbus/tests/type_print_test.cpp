a299a6f3e89bc6dd9ab8f78e7e16b7ae40216eb1
Michael Spieler <michael.spieler@epfl.ch>
2016-11-20 01:48:27 +0100

    test type print with type compiler

--- src/msgbus/tests/type_print_test.cpp
+++ src/msgbus/tests/type_print_test.cpp
@@ -1,76 +1,7 @@
 #include <CppUTest/TestHarness.h>
 #include "../type_definition.h"
 #include "../type_print.h"
-
-typedef struct simple_s {
-    float x;
-    int32_t y;
-    char str[20+1];
-} simple_t;
-
-const static msgbus_type_entry_t simple_entries[] = {
-    {
-        .name = "x",
-        .is_base_type = 1,
-        .is_array = 0,
-        .is_dynamic_array = 0,
-        .struct_offset = offsetof(simple_t, x),
-        .base_type = MSGBUS_TYPE_FLOAT32,
-    },
-    {
-        .name = "y",
-        .is_base_type = 1,
-        .is_array = 0,
-        .is_dynamic_array = 0,
-        .struct_offset = offsetof(simple_t, y),
-        .base_type = MSGBUS_TYPE_INT32,
-    },
-    {
-        .name = "str",
-        .is_base_type = 1,
-        .is_array = 0,
-        .is_dynamic_array = 0,
-        .struct_offset = offsetof(simple_t, str),
-        .base_type = MSGBUS_TYPE_STRING,
-        .size=20+1
-    }
-};
-
-const static msgbus_type_definition_t simple_type = {
-    .nb_elements = 3,
-    .elements = simple_entries,
-    .struct_size = sizeof(simple_t),
-};
-
-typedef struct nested_s {
-    int32_t x;
-    simple_t simple;
-} nested_t;
-
-const static msgbus_type_entry_t nested_entries[] = {
-    {
-        .name = "x",
-        .is_base_type = 1,
-        .is_array = 0,
-        .is_dynamic_array = 0,
-        .struct_offset = offsetof(simple_t, x),
-        .base_type = MSGBUS_TYPE_INT32,
-    },
-    {
-        .name = "simple",
-        .is_base_type = 0,
-        .is_array = 0,
-        .is_dynamic_array = 0,
-        .struct_offset = offsetof(nested_t, simple),
-        .type = &simple_type
-    }
-};
-
-const static msgbus_type_definition_t nested_type = {
-    .nb_elements = 2,
-    .elements = nested_entries,
-    .struct_size = sizeof(nested_t),
-};
+#include "types/test.h"
 
 extern "C"
 void print_fn(void *p, const char *fmt, ...)
@@ -151,63 +82,24 @@ TEST(TypePrintTestGroup, CanPrintString)
 
 TEST(TypePrintTestGroup, CanPrintSimpleType)
 {
-    simple_t object = {.x = 1.5f, .y = 42, .str = "hello world!"};
+    simple_t object = {.x = 1.5f, .y = 42};
     msgbus_print_type(print_fn, &arg, &simple_type, &object);
     STRCMP_EQUAL(
         "x: 1.500000\n"
-        "y: 42\n"
-        "str: \"hello world!\"\n",
+        "y: 42\n",
         buffer);
 }
 
 TEST(TypePrintTestGroup, CanPrintNestedType)
 {
-    nested_t object = {.x = 42, .simple = {.x = 1.0f, .y = 123, .str = "foo"}};
+    nested_t object = {.s = {.x = 1.0, .y = 123}};
     msgbus_print_type(print_fn, &arg, &nested_type, &object);
     STRCMP_EQUAL(
-        "x: 42\n"
-        "simple:\n"
+        "s:\n"
         "    x: 1.000000\n"
-        "    y: 123\n"
-        "    str: \"foo\"\n",
+        "    y: 123\n",
         buffer);
 }
-
-typedef struct {
-    int32_t x[4];
-    uint16_t x_len;
-    char strings[2][10];
-} arrays_t;
-
-const static msgbus_type_entry_t arrays_entries[] = {
-    {
-        .name = "x",
-        .is_base_type = 1,
-        .is_array = 0,
-        .is_dynamic_array = 1,
-        .array_len = 4,
-        .dynamic_array_len_struct_offset = offsetof(arrays_t, x_len),
-        .struct_offset = offsetof(arrays_t, x),
-        .base_type = MSGBUS_TYPE_INT32,
-        .size = sizeof(int32_t)
-    },
-    {
-        .name = "strings",
-        .is_base_type = 1,
-        .is_array = 1,
-        .is_dynamic_array = 0,
-        .array_len = 2,
-        .struct_offset = offsetof(arrays_t, strings),
-        .base_type = MSGBUS_TYPE_STRING,
-        .size = 10
-    }
-};
-
-const static msgbus_type_definition_t arrays_type = {
-    .nb_elements = 2,
-    .elements = arrays_entries,
-    .struct_size = sizeof(arrays_t),
-};
 
 TEST(TypePrintTestGroup, CanPrintArrayType)
 {
@@ -219,50 +111,24 @@ TEST(TypePrintTestGroup, CanPrintArrayType)
         buffer);
 }
 
-typedef struct {
-    simple_t arr[3];
-} arrays_of_structs_t;
-
-const static msgbus_type_entry_t arrays_of_structs_entries[] = {
-    {
-        .name = "arr",
-        .is_base_type = 0,
-        .is_array = 1,
-        .is_dynamic_array = 0,
-        .array_len = 3,
-        .struct_offset = offsetof(arrays_of_structs_t, arr),
-        .type = &simple_type,
-        .size = sizeof(simple_t)
-    },
-};
-
-const static msgbus_type_definition_t arrays_of_structs_type = {
-    .nb_elements = 1,
-    .elements = arrays_of_structs_entries,
-    .struct_size = sizeof(arrays_of_structs_t),
-};
-
 TEST(TypePrintTestGroup, CanPrintArraysOfStructs)
 {
     arrays_of_structs_t object = {.arr = {
-        {.x = 0.0f, .y = 0, .str = "a"},
-        {.x = 1.0f, .y = 1, .str = "b"},
-        {.x = 2.0f, .y = 2, .str = "c"}
+        {.x = 0.0f, .y = 0},
+        {.x = 1.0f, .y = 1},
+        {.x = 2.0f, .y = 2}
     }};
     msgbus_print_type(print_fn, &arg, &arrays_of_structs_type, &object);
     STRCMP_EQUAL(
         "arr: [\n"
         "    x: 0.000000\n"
         "    y: 0\n"
-        "    str: \"a\"\n"
         "\n"
         "    x: 1.000000\n"
         "    y: 1\n"
-        "    str: \"b\"\n"
         "\n"
         "    x: 2.000000\n"
         "    y: 2\n"
-        "    str: \"c\"\n"
         "]\n",
         buffer);
 }
