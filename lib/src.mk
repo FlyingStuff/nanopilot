6545761b56d3ca0c6a4ec68c8fb043df876ab0bb
Patrick Spieler <stapelzeiger@gmail.com>
2018-03-22 20:36:51 -0700

    move parameter from src to lib

--- lib/src.mk
+++ lib/src.mk
@@ -25,6 +25,9 @@ PROJCSRC += $(PROJROOT)/lib/comm/datagram-messages/msg_dispatcher.c
 PROJCSRC += $(PROJROOT)/lib/comm/datagram-messages/service_call.c
 PROJCSRC += $(PROJROOT)/lib/comm/serial-datagram/serial_datagram.c
 PROJCSRC += $(PROJROOT)/lib/comm/serial-datagram/serial_datagram_buffer_writer.c
+PROJCSRC += $(PROJROOT)/lib/parameter/parameter.c
+PROJCSRC += $(PROJROOT)/lib/parameter/parameter_msgpack.c
+PROJCSRC += $(PROJROOT)/lib/parameter/parameter_print.c
 
 PROJCSRC += $(PROJROOT)/lib/mcucom/msgbus/msgbus.c
 PROJCSRC += $(PROJROOT)/lib/mcucom/port/ChibiOS/mcucom_port_sync.c
